c9d37842d55b0a5bd5d64f87c92a66b9141e3ac4
SMakela <73409799+smakela13@users.noreply.github.com>
2021-05-07 19:35:18 -0400

    Completed the POST, PUT, and DELETE routes for category-routes and then finished the GET routes and DELETE route for product-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,10 +13,10 @@ router.get('/', (req, res) => {
         attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
       }]
   }).then(categoryData => res.json(categoryData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    })
+    .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
@@ -36,7 +36,7 @@ router.get('/:id', (req, res) => {
       return;
     }
     res.json(categoryData)
-  }).catch(err => {
+  }).catch((err) => {
     console.log(err);
     res.status(500).json(err);
   });
@@ -44,14 +44,49 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  }).then(categoryData => res.json(categoryData))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(categoryData => {
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No entry found' });
+      return;
+    }
+    res.json(categoryData)
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({ message: 'No entry found' });
+      return;
+    }
+    res.json(categoryData)
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,41 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
   // be sure to include its associated Category and Tag data
+    include: [{
+      model: Category,
+      attributes: ['id', 'category_name']
+    },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+    }]
+}).then(productData => res.json(productData))
+    .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
   // be sure to include its associated Category and Tag data
+    include: [{
+      model: Category,
+      attributes: ['id', 'category_name']
+    },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+    }]
+}).then(productData => res.json(productData))
+    .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -91,6 +119,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy(req.body, {
+      where: {
+        id: req.params.id
+      }
+    }).then(productData => {
+      if (!productData) {
+        res.status(404).json({ message: 'No entry found' });
+        return;
+      }
+      res.json(productData)
+    }).catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
